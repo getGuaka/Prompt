1ba363ec60736214e38888e5df6e0f34046a8ac1
Thi <t@thi.im>
2018-10-03 02:29:21 +0900

    Make PromptSettings public

--- Sources/Prompt/PromptSettings.swift
+++ Sources/Prompt/PromptSettings.swift
@@ -7,16 +7,16 @@
 //
 
 
-class PromptSettings {
+public class PromptSettings {
 
-    static var reader: PromptReader = ConsolePromptReader()
-    static var printer: PromptPrinter = ConsolePromptPrinter()
+    public static var reader: PromptReader = ConsolePromptReader()
+    public static var printer: PromptPrinter = ConsolePromptPrinter()
 
-    class func read() -> String? {
+    public class func read() -> String? {
         return reader.read()
     }
 
-    class func print(_ string: String, terminator: String = "\n") {
+    public class func print(_ string: String, terminator: String = "\n") {
         return printer.printString(string, terminator: terminator)
     }
 }
